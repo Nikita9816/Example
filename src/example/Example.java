ad1d6b97a50452e6eea1ab01f6bac0816cff256b
Nikita Tsistokletov <tsistokletovnikita05@gmail.com>
2022-10-03 20:18:06 +0300

    first commit

---
+++ src/example/Example.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tsist
+ */
+public class Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Input your name");
+        //String user = scanner.nextLine();
+        //System.out.println("Hello" + " " + user);
+        //System.out.println("");
+        //System.out.println("Put");
+        //int num = scanner.nextInt();
+        //System.out.println("Put one more");
+        //byte nnum = scanner.nextByte();
+        //System.out.println("Result is  " + num + nnum);
+        //System.out.println("Put first number");
+        //float num = scanner.nextInt();
+        //System.out.println("Put second number");
+        //float num2 = scanner.nextInt();
+        //float res1 = num + num2;
+        //float res2 = num - num2;
+        //float res3 = num / num2;
+        //float res4 = num * num2;
+        //System.out.println("Result");
+        //System.out.println("");
+        //System.out.println("Result = " + res1 + "\n" + "Result = " + res2 + "\n" + "Result = " + res3 +"\n" + "Result = " + res4);
+         //System.out.println("Put a number ");
+         //int num = scanner.nextInt();
+         //System.out.println("Put a second number ");
+         //int num1 = scanner.nextInt();
+         //int result = num + num1;
+         //System.out.println("Your result is = " + result);
+         
+         
+        //int a = 15, b = 10; 
+        char sym = '5', sym2 = '4';
+        if(sym==sym2){
+            System.out.println("Yes");
+        
+        }else{
+            System.out.println("No");
+        }
+    }
+}
+
+         
+         
+         
+         
+         
+         
+         
+        
+        
+        
+        
+        
+        
+    
+    
+
